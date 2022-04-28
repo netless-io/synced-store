490d10efd19d240e72ac6bfa22a46e97d399fb97
CRIMX <straybugs@gmail.com>
2022-04-28 22:39:45 +0800

    chore: init project

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,23 @@
-/* eslint-disable import/no-anonymous-default-export */
+/* eslint-env node */
+
 import path from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command, mode }) => {
-    const isProd = mode === "production";
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
 
-    return {
-        build: {
-            lib: {
-                entry: path.resolve(__dirname, "src/index.ts"),
-                formats: ["es", "cjs", "umd"],
-                name: "SyncedStore",
-                fileName: "index"
-            },
-            outDir: "dist",
-            sourcemap: false,
-            rollupOptions: {
-                external: ["white-web-sdk"],
-            },
-            minify: true,
-        },
-    };
+  return {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        formats: ["es", "cjs"],
+      },
+      outDir: "dist",
+      sourcemap: isProd,
+      rollupOptions: {
+        external: ["white-web-sdk"],
+      },
+      minify: isProd,
+    },
+  };
 });
